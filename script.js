15b795d828470800ebb788b38744e831577987cf
nataliereshetnikova <ab1748@gmail.com>
2019-02-13 17:39:55 +0200

    initial commit no js

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", function () {
+    var f = document.myform;
+    var size = f.size.value;
+    var clientsName = f.ClientsName.value;
+    var clientsEmail = f.ClientsEmail.value;
+    var clientsAdress = f.ClientsAdress.value;
+    var clientsPhone = f.ClientsPhone.value;
+    var cheese = f.cheese.value;
+    var becon = f.becon.value;
+    var sausage = f.sausage.value;
+    var mushrooms = f.mushrooms.value;
+    var olives = f.olives.value;
+    var spices = f.spices.value;
+
+}
